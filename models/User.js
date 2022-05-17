6ed2240b1ad267b354d7638d3fd5ed56754ae645
Sharryy <ibneadam388@gmail.com>
2022-05-17 20:04:05 +0500

    feat:Create User Model and Controller

---
+++ models/User.js
@@ -0,0 +1,27 @@
+const mongoose = require('mongoose');
+
+const UserSchema = new mongoose.Schema({
+        name: {
+            type: String,
+            trim: true,
+            required: true
+        },
+        email: {
+            type: String,
+            trim: true,
+            required: true
+        },
+        password: {
+            type: String,
+            trim: true,
+            required: true
+        },
+        phone: {
+            type: String,
+            trim: true,
+            required: true
+        },
+    }, {timestamps: true}
+);
+
+module.exports = mongoose.model('User', UserSchema);
