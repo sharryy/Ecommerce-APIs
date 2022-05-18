643a63814494135e3ab73854a996e0db94209460
Sharryy <ibneadam388@gmail.com>
2022-05-18 15:11:11 +0500

    feat:Create Category API Done.

---
+++ controllers/CategoryController.js
@@ -0,0 +1,17 @@
+const Category = require('../models/Category');
+
+if (process.env.NODE_ENV === 'production') {
+    require('dotenv').config();
+}
+
+exports.createCategory = async function (req, res, next) {
+    //if category with same name already exists, return error
+    const category = await Category.findOne({name: req.body.name});
+    if (category) return res.status(400).json({message: 'Category with this name already exists'});
+
+    const newCategory = new Category({name: req.body.name});
+    newCategory.save((error, success) => {
+        if (error) return res.status(500).json({message: "Error creating category. Please try again later."});
+        return res.status(200).json({message: "Category created successfully", category: success});
+    });
+}
