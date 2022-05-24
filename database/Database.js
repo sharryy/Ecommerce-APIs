9a41f52cd7b0a998f5a1779521ed5626420a69d4
Sharryy <ibneadam388@gmail.com>
2022-05-24 18:17:17 +0500

    chore:Update DB URL

--- database/Database.js
+++ database/Database.js
@@ -2,7 +2,7 @@ const {connect} = require("mongoose");
 require("dotenv").config();
 
 const database = connect(
-    process.env.DATABASE_URL,
+    process.env.DATABASE_URL_LIVE,
     {
         useNewUrlParser: true,
         useUnifiedTopology: true,
